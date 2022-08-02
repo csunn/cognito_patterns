3d1d047b1f0d26082c671ac7adbf96c204a4de7f
csunn <110186615+csunn@users.noreply.github.com>
2022-08-01 23:13:30 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,192 @@
-/* import by name */
+// import React, { useState } from "react";
+// import { Auth, Amplify, Hub } from "aws-amplify";
+// import {
+//   TextField,
+//   Button,
+//   Heading,
+//   PhoneNumberField,
+//   Authenticator,
+// } from "@aws-amplify/ui-react";
+// import "@aws-amplify/ui-react/styles.css";
+
+// /* aws-exports configures cognito and allows connection to aws amplify within my aws account*/
+// import awsExports from "aws-exports";
+
+// import Home from "Home"; //*importing our homepage attributes from Home.tsx
+
+// Amplify.configure(awsExports); // setting up the various AWS resources that we want to work with
+
+// // storing function using const instead of function()
+// // makes function immutable & allows for fat arrow syntax
+// const App = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [first_name, setFirstName] = useState("");
+//   const [last_name, setLastName] = useState("");
+//   const [email, setEmail] = useState("");
+
+//   const handleSubmit = () => {
+//     Auth.signUp({
+//       username,
+//       password,
+//       attributes: {
+//         first_name,
+//         last_name,
+//         email,
+//       },
+//       autoSignIn: {
+//         enabled: true,
+//       },
+//     });
+//   };
+
+//   // const listenToAutoSignInEvent=() =>{
+//   //   Hub.listen("auth", ({payload})) => {
+//   //     const { event } = payload;
+//   //     if (event == "autoSignIn") {
+//   //       const user = payload.data;
+//   //     }
+//   //   }
+//   // };
+//   return (
+//     <form>
+//       <Heading level={1} fontWeight="bold">
+//         Sign-Up
+//       </Heading>
+
+//       <TextField
+//         name="username"
+//         placeholder="Username"
+//         label="Username"
+//         onChange={(event) => {
+//           // console.log(event.target.value); update username after every type change
+//           setUsername(event.target.value);
+//         }}
+//       />
+//       <TextField
+//         name="first_name"
+//         placeholder="First Name"
+//         label="First Name"
+//         onChange={(event) => {
+//           setFirstName(event.target.value);
+//         }}
+//       />
+//       <TextField
+//         name="last_name"
+//         placeholder="Last Name"
+//         label="Last Name"
+//         onChange={(event) => {
+//           setLastName(event.target.value);
+//         }}
+//       />
+//       <TextField
+//         name="email"
+//         placeholder="Email"
+//         label="Email"
+//         onChange={(event) => {
+//           setEmail(event.target.value);
+//         }}
+//       />
+//       <PhoneNumberField
+//         name="phone_number"
+//         defaultCountryCode="+1"
+//         label="Phone Number"
+//         placeholder="XXX-XXX-XXXX"
+//       />
+//       <TextField
+//         name="password"
+//         type="password"
+//         placeholder="Password"
+//         label="Password"
+//         onChange={(event) => setPassword(event.target.value)}
+//       />
+//       <TextField
+//         name="comfirm_password"
+//         type="password"
+//         placeholder="Confirm Password"
+//         label="Confirm Password"
+//         onChange={(event) => setPassword(event.target.value)}
+//       />
+//       <Button loadingText="" onClick={handleSubmit} ariaLabel="">
+//         Sign Up
+//       </Button>
+//     </form>
+//   );
+// };
+
+// export default App;
+
+// // import React, { useState } from "react";
+// // import { Auth, Amplify, Hub } from "aws-amplify";
+// // import {
+// //   TextField,
+// //   Button,
+// //   Heading,
+// //   PhoneNumberField,
+// //   withAuthenticator,
+// // } from "@aws-amplify/ui-react";
+// // import "@aws-amplify/ui-react/styles.css";
+
+// // import awsExports from "aws-exports";
+
+// // import Home from "Home";
+// // import "@aws-amplify/ui-react/styles.css";
+// // Amplify.configure(awsExports);
+
+// // const App = () => {
+// //   const [username, setUsername] = useState("");
+// //   const [password, setPassword] = useState("");
+
+// //   const handleSubmit = () => {
+// //     Auth.signIn(username);
+// //   };
+// //   <form>
+// //     <TextField
+// //       name="username"
+// //       placeholder="Username"
+// //       label="Username"
+// //       onChange={(event) => {
+// //         console.log(event.target.value);
+// //         setUsername(event.target.value);
+// //       }}
+// //     />
+// //     <TextField
+// //       name="password"
+// //       type="password"
+// //       placeholder="Password"
+// //       label="Password"
+// //     />
+
+// //     <Button loadingText="" onClick={handleSubmit} ariaLabel="">
+// //       Sign In
+// //     </Button>
+// //   </form>;
+// // };
+
+// // export default App;
+
 import { Amplify } from "aws-amplify";
-import { Authenticator, useAuthenticator, CheckboxField } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from "@aws-amplify/ui-react";
 
-/* aws-exports configures cognito and allows connection to aws amplify within my aws account*/
-import awsExports from 'aws-exports'; 
+import awsExports from "aws-exports";
 
-import Home from "Home"; //*importing our homepage attributes from Home.tsx
-
-Amplify.configure(awsExports); // setting up the various AWS resources that we want to work with
-
+Amplify.configure(awsExports);
 
 const formFields = {
-   signUp: {
+  signIn: {
     username: {
-      order: 1,
-      placeholder: "Username"
+      labelHidden: false,
+      placeholder: "Username",
+      isRequired: true,
+      label: "Username:",
     },
-     email: {
-       order:1
-     },
-     first_name: {
-       order: 2,
-       placeholder: 'First Name'
-     },
-     last_name: {
-      order: 3,
-      placeholder: 'Last Name'
-     },
-     password: {
-       order: 4
-     },
-     confirm_password: {
-       order: 5
-     },
-   },
-  }
+  },
+};
 
-const App = () => {
+export default function App() {
   return (
-    <Authenticator 
-      initialState="signIn" 
-      components={{
-        SignUp: {
-          FormFields() {
-            const { validationErrors } = useAuthenticator();
-
-            return (
-              
-              <>
-
-                <Authenticator.SignUp.FormFields/>
-
-
-                <CheckboxField
-                  errorMessage={validationErrors.acknowledgement as string}
-                  hasError={!!validationErrors.acknowledgement}
-                  name="acknowledgement"
-                  value="yes"
-                  label="I agree with the Terms & Conditions."
-                />
-              </>
-            );
-          },
-        },
-      }}
-      formFields={formFields}
-      
-
-      services={{ 
-        async validateCustomSignUp(formData) {
-          if (!formData.acknowledgement) {
-            return {
-              acknowledgement: 'You must agree to the Terms & Conditions',
-            };
-          }
-        },
-      }}
-    >
-
-
-      {({ signOut, user }) => (
-          <Home myUser={user} userSignout={signOut} />
-      )}
+    <Authenticator formFields={formFields}>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
     </Authenticator>
   );
 }
-export default App;
